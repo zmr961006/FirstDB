91e962d3399b49ae509de2118ece08c29c4a6322
wrx1721267362 <1721267632@qq.com>
2016-03-24 15:51:03 +0800

    build env

---
+++ Src/FDB_String.h
@@ -0,0 +1,67 @@
+/*************************************************************************
+	> File Name: FDB_string.h
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年03月23日 星期三 08时23分40秒
+ ************************************************************************/
+
+#ifndef _FDB_STRING_H
+#define _FDB_STRING_H
+
+#include<iostream>
+#include<cstring>
+#include<cstdlib>
+#include<cassert>
+
+#define STRING_MAX  1 << 12		//字符串最大长度
+#define LEN(n)      (n * 2 + 1)		//初始化时整体长度
+#define FREE(n)     (LEN(n) - n)	//预分配长度
+
+#define DEFAULT_NULL 	0		//默认空字符串长度
+#define  DEFAULT_SIZE   (1<<7) 		//默认数组长度
+
+class String
+{
+    //重载部分运算符
+    //重载 '>','<','==','!='关系运算符
+    friend bool operator<(const String &lhs, const String &rhs);
+    friend bool operator>(const String &lhs, const String &rhs);
+    friend bool operator==(const String &lhs, const String &rhs);
+    friend bool operator!=(const String &lhs, const String &rhs);
+    //重载输入输出运算符
+    friend std::ostream& operator<<(std::ostream &os, const String &rhs);
+    friend std::istream& operator>>(std::istream &is, String &rhs);
+    //重载相加运算符
+    friend String operator+(const String &lhs, const String &rhs);
+    friend String operator+(const String &lhs, const char *rhs);
+
+private:
+    int size; 		//字符串已用长度
+    int free;		//字符串已经使用长度
+    char *str; 		//空闲长度
+public:
+    String(); 				//无参构造
+    String(char *rhs); 			//行参为char* 构造函数
+    String(const String &rhs); 		//复制构造函数
+    ~String();  			//析构函数
+
+    //重载运算符
+    String & operator=(const String &rhs); 	//赋值运算符
+    String & operator=(const char *rhs); 	//行参为char* 的赋值运算符
+    char& operator[](int i);			//下标运算符
+    String& operator+=(const String &rhs); 	//复合赋值运算符
+    String& operator+=(const char *rhs); 	//行参为char *的复合赋值运算符
+
+    //接口函数
+    int getsize() const; 				//获取当前字符串已用空间
+    int getfree() const; 				//获取字符串空闲空间
+    void Print(); 					//打印字符串
+    String &StringAdd(const String &str2); 		//向字符串添加String类型内容
+    String &StringAdd(const char *s); 			//向字符串添加char *类型的内容
+    String &StringClear(); 				//清除字符串内容
+    String &StringSizeClear(size_t pos, size_t len); 	//以pos开始清除之后len长度的内容
+    String &StringChange(const char *s); 		//修改字符串为s
+    String &StringChange(const String &str2); 		//修改字符串为str2
+};
+
+#endif
---
+++ Src/FDB_String_Ceshi.cpp
@@ -0,0 +1,42 @@
+/*************************************************************************
+	> File Name: a.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年03月24日 星期四 10时52分30秒
+ ************************************************************************/
+
+#include<iostream>
+#include"./FDB_String.h"
+#include"./FDB_String.cpp"
+using namespace std;
+
+int main()
+{
+    String a("ssssssss");
+    String b;
+    char c[10] = "doubiias";
+
+    cout << a << "  size : " << a.getsize() << " free : " << a.getfree() << endl;
+    cin >> b;
+    cout << b << "  size : " << b.getsize() << " free : " << b.getfree() << endl;
+    b = a;
+    cout << b << "  size : " << b.getsize() << " free : " << b.getfree() << endl;
+    b[0] = '1';
+    cout << b << "  size : " << b.getsize() << " free : " << b.getfree() << endl;
+    a += b;
+    cout << a << "  size : " << a.getsize() << " free : " << a.getfree() << endl;
+    cout << (a > b) << " " << (a < b) << " " << (a == b) <<endl;
+    a += c;
+    cout << a << "  size : " << a.getsize() << " free : " << a.getfree() << endl;
+    cout << b << "  size : " << b.getsize() << " free : " << b.getfree() << endl;
+    String d = a + b;
+    cout << d << "  size : " << d.getsize() << " free : " << d.getfree() << endl;
+    d = a + c;
+    cout << d << "  size : " << d.getsize() << " free : " << d.getfree() << endl;
+    b.StringSizeClear(1,2);
+    cout << b << "  size : " << b.getsize() << " free : " << b.getfree() << endl;
+    d.StringClear(); 
+    cout << d << "  size : " << d.getsize() << " free : " << d.getfree() << endl;
+
+    return 0;
+}
