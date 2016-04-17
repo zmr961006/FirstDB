228cc03d76ce2cf02a615e3218681f4475b7208a
wrx1721267362 <1721267632@qq.com>
2016-04-17 19:13:16 +0800

    LOG create

--- Src/test/FDB_LOG_test.cpp
+++ Src/test/FDB_LOG_test.cpp
@@ -16,11 +16,6 @@
 using namespace std;
 
 FDB_LOG a;
-/*pthread_t th1,th2,th3,th4;
-pthread_cond_t cond1;
-pthread_cond_t cond2;
-pthread_cond_t cond3;
-pthread_cond_t cond4;*/
 
 void thread_1()
 {
@@ -51,24 +46,4 @@ int main()
     t2.join();
     t3.join();
     t4.join();
-
-    /*char b[255];
-    int i;
-    for( i = 0; i < 250; i++)
-    {
-        b[i] = '1';
-    }
-    b[i] = 0;
-
-    for (int i = 0; i < 1000000; i++)
-    {
-        a.FDB_LOG_ADD(b, 250);
-    }
-
-    a.FDB_LOG_Write();*/ 
-
-    /*pthread_create(&th1, NULL, thread11, NULL);
-    pthread_create(&th2, NULL, thread11, NULL);
-    pthread_create(&th3, NULL, thread11, NULL);
-    pthread_create(&th4, NULL, thread11, NULL);*/
 }
